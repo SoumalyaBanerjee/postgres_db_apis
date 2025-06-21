62040a80b89a280121ae8594351f76b2089169c5
SoumalyaBanerjee <40776477+SoumalyaBanerjee@users.noreply.github.com>
2025-06-21 21:53:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from typing import Optional
 from fastapi import HTTPException
 from pydantic import BaseModel, validator
+from psycopg2.extras import RealDictCursor
+
 
 
 app = FastAPI()
